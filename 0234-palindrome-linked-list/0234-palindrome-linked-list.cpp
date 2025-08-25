9267a415b53ace9207c24d63cb989f6b0d5c82da
Saumya Dhorje <saumya.dhorje24@vit.edu>
2025-08-25 22:45:52 +0530

    0234-PALINDROME-LINKED-LIST Stats: Time: 2 ms (66.53%), Space: 132.1 MB (10.42%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -30,3 +30,32 @@ public:
         return true;
     }
 };
+//2nd sol
+ // bool isPalindrome(ListNode* head) {
+  //  ListNode *slow = head, *fast = head;
+  //  ListNode *next, *prev = new ListNode();
+
+    //while(fast && fast->next) {
+   //   // for finding middle
+    //  slow = slow->next;
+    //  fast = fast->next->next; 
+
+      // reversing first half
+   //   next = head->next;
+   //   head->next = prev;
+   //   prev = head;
+   //   head = next;
+   // }
+ 
+    // for edge cases
+   // if(fast) slow = slow->next;
+   // head = prev;
+
+    // comparing first half to second one
+  //  while(slow) {
+  //    if(head->val != slow->val) return false;
+  //    head = head->next, slow = slow->next;
+  //  }
+ //   return true;
+ // }
+//};
